315b787e14186efde4a10b29519efb5e65e0e05c
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-15 08:36:51 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
-
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
