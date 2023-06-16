7ca45ae22b494d2637b1c92da7f87697f1a141a2
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-16 21:38:35 +0000

    a

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Write a program that prints all possible
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int firstDigit = 0, secondDigit;
+
+	while (firstDigit <= 99)
+	{
+		secondDigit = firstDigit;
+		while (secondDigit <= 99)
+		{
+			if (secondDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((secondDigit / 10) + 48);
+				putchar((secondDigit % 10) + 48);
+
+				if (firstDigit != 98 || secondDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			secondDigit++;
+		}
+		firstDigit++;
+	}
+	putchar('\n');
+	return (0);
+}
