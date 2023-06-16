e17eea11ab76e3ba695c647411c19fb3e371846d
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-16 16:03:04 +0000

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,29 +9,29 @@
  */
 int main(void)
 {
-    int digit1 = 0, digit2;
+	int digit1 = 0, digit2;
 
-    while (digit1 <= 9)
-    {
-        digit2 = 0;
-        while (digit2 <= 9)
-        {
-            if (digit1 != digit2 && digit1 < digit2)
-            {
-                putchar(digit1 + '0');
-                putchar(digit2 + '0');
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digit1 != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
 
-                if (!(digit1 == 8 && digit2 == 9))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-            digit2++;
-        }
-        digit1++;
-    }
-    putchar('\n');
+				if (!(digit1 == 8 && digit2 == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
