f3b2c7d7ec64ce9e07321982e6e71e26c21fb877
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-16 14:54:31 +0000

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 9 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/* convert digit to ascii */
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
