f46c0db6ec02eb6e45b48b1c5f88a6d5941af452
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-20 22:59:08 +0000

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,46 +1,51 @@
 #include <stdio.h>
 
-#define MAX_INDEX 98
-
 /**
- * main - prints first 98 Fibonacci numbers, starting with 1 and 2.
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
-	unsigned long int a1 = 1, a2 = 0, b1 = 2, b2 = 0, next1, next2;
-	int count;
+	int loopCounter;
+	unsigned long fibNum1 = 0, fibNum2 = 1, fibSum;
+	unsigned long fibNum1_half1, fibNum1_half2, fibNum2_half1, fibNum2_half2;
+	unsigned long halfSum1, halfSum2;
 
-	printf("%lu, %lu, ", a1, b1);
-
-	for (count = 3; count <= 98; count++)
+	for (loopCounter = 0; loopCounter < 92; loopCounter++)
 	{
-		next2 = a2 + b2;
-		next1 = a1 + b1 + (next2 / 1000000000);
-		next2 %= 1000000000;
+		fibSum = fibNum1 + fibNum2;
+		printf("%lu, ", fibSum);
 
-		if (count != 98)
-		{
-			if (next2 > 0)
-				printf("%lu%09lu, ", next1, next2);
-			else
-				printf("%lu, ", next1);
-		}
-		else
-		{
-			if (next2 > 0)
-				printf("%lu%09lu\n", next1, next2);
-			else
-				printf("%lu\n", next1);
-		}
-
-		a1 = b1;
-		a2 = b2;
-		b1 = next1;
-		b2 = next2;
+		fibNum1 = fibNum2;
+		fibNum2 = fibSum;
 	}
 
+	fibNum1_half1 = fibNum1 / 10000000000;
+	fibNum2_half1 = fibNum2 / 10000000000;
+	fibNum1_half2 = fibNum1 % 10000000000;
+	fibNum2_half2 = fibNum2 % 10000000000;
+
+	for (loopCounter = 93; loopCounter < 99; loopCounter++)
+	{
+		halfSum1 = fibNum1_half1 + fibNum2_half1;
+		halfSum2 = fibNum1_half2 + fibNum2_half2;
+		if (fibNum1_half2 + fibNum2_half2 > 9999999999)
+		{
+			halfSum1 += 1;
+			halfSum2 %= 10000000000;
+		}
+
+		printf("%lu%lu", halfSum1, halfSum2);
+		if (loopCounter != 98)
+			printf(", ");
+
+		fibNum1_half1 = fibNum2_half1;
+		fibNum1_half2 = fibNum2_half2;
+		fibNum2_half1 = halfSum1;
+		fibNum2_half2 = halfSum2;
+	}
+	printf("\n");
 	return (0);
 }
