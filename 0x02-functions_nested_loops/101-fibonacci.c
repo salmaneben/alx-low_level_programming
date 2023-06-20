402c4e8bf39a924413931c042ada3459103b1d6e
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-20 18:07:45 +0000

    a

--- 0x02-functions_nested_loops/101-fibonacci.c
+++ 0x02-functions_nested_loops/101-fibonacci.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the sum of all multiples of 3 or 5  to 1024
+  * main - Prints the sum of all multiples of 3 or 5 up to 1024
   *
   * Return: Always (Success)
   */
 int main(void)
 {
-	int i, a = 0;
+	int i, z = 0;
 
 	while (i < 1024)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			a += i;
+			z += i;
 		}
 
 		i++;
 	}
 
-	printf("%d\n", a);
+	printf("%d\n", z);
 	return (0);
 }
