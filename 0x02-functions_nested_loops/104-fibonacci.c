ecec7885cdefe41c5d70abc6bd8ccf7b64dbc236
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-20 21:36:31 +0000

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define MAX_INDEX 98
+
 /**
  * main - prints first 98 Fibonacci numbers, starting with 1 and 2.
  *
@@ -7,19 +9,22 @@
  */
 int main(void)
 {
-	long double i = 1, j = 2, next;
+	unsigned long f1 = 1, f2 = 2, next;
 	int count;
 
-	printf("%.0Lf, %.0Lf", i, j);
+	printf("%lu, %lu, ", f1, f2);
 
-	for (count = 3; count <= 98; count++)
+	for (count = 3; count <= MAX_INDEX; count++)
 	{
-		next = i + j;
-		i = j;
-		j = next;
-		printf(", %.0Lf", next);
+		next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+
+		if (count != MAX_INDEX)
+			printf("%lu, ", next);
+		else
+			printf("%lu\n", next);
 	}
-	printf("\n");
 
 	return (0);
 }
