093ce18ba91f7c3d81f92e6e48c9484a9399d4c9
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-20 21:39:16 +0000

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,24 +6,40 @@
  * main - prints first 98 Fibonacci numbers, starting with 1 and 2.
  *
  * Return: Always 0.
- */
+*/
+
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, next;
+	unsigned long int a1 = 1, a2 = 0, b1 = 2, b2 = 0, next1, next2;
 	int count;
 
-	printf("%lu, %lu, ", f1, f2);
+	printf("%lu, %lu, ", a1, b1);
 
-	for (count = 3; count <= MAX_INDEX; count++)
+	for (count = 3; count <= 98; count++)
 	{
-		next = f1 + f2;
-		f1 = f2;
-		f2 = next;
+		next2 = a2 + b2;
+		next1 = a1 + b1 + (next2 / 1000000000);
+		next2 %= 1000000000;
 
-		if (count != MAX_INDEX)
-			printf("%lu, ", next);
+		if (count != 98)
+		{
+			if (next2 > 0)
+				printf("%lu%09lu, ", next1, next2);
+			else
+				printf("%lu, ", next1);
+		}
 		else
-			printf("%lu\n", next);
+		{
+			if (next2 > 0)
+				printf("%lu%09lu\n", next1, next2);
+			else
+				printf("%lu\n", next1);
+		}
+
+		a1 = b1;
+		a2 = b2;
+		b1 = next1;
+		b2 = next2;
 	}
 
 	return (0);
