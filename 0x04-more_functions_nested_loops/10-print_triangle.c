a2fca2aa9a99278d948a116c3556db88a51bce4b
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-22 11:35:21 +0000

    a

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * print_triangle - Prints a triangle of squares according parameter
+  * @size: The size of the squares triangle
+  *
+  * Return: empty
+  */
+
+void print_triangle(int size)
+{
+	int r, h, s;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 1; r <= size; r++)
+		{
+			for (s = size - r; s >= 1; s--)
+			{
+				_putchar(' ');
+			}
+			for (h = 1; h <= r; h++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
