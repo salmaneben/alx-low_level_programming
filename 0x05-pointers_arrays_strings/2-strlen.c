c9ce7558570837014a3d51b1e2e4d8e4370e95cd
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-26 13:02:02 +0000

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int q = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		q++;
+	}
+
+	return (c);
+}
