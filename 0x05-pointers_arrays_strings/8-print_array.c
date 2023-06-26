6b61006cb82d22e416249f99c54f6776aca428a1
salmaneben <salmanebenyakhlaf95@gmail.com>
2023-06-26 13:24:58 +0000

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n element of an array of integers
+ * Owned by salmane
+ * @a: int to check
+ * @n: int to check
+ * Return: 0 is success
+ */
+void print_array(int *a, int n)
+{
+	int z;
+
+	for (z = 0; x < n; z++)
+		if (z != n - 1)
+		printf("%d, ", a[z]);
+		else
+			printf("%d", a[z]);
+printf("\n");
+}
